49566e1f4c034e344f80a19584345627b7f511a3
Joan Lima <joanlima95@gmail.com>
2024-02-24 20:30:00 -0300

    add ec2 test instance

--- main.tf
+++ main.tf
@@ -4,11 +4,30 @@ provider "aws" {
 }
 
 resource "aws_instance" "backend" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ubuntu_sec_group.id]
+
+  user_data = <<-EOF
+              #!bin/bash
+              echo "hello instance!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     name = "ubuntu-instance"
+  }
+
+}
+
+resource "aws_security_group" "ubuntu_sec_group" {
+
+  name = "ubuntu-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
